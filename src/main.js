19df9bc56fd2971a6d6169c20ec59efd3f338ad4
delliCC <desarrollo.web.chontalpa@gmail.com>
2022-11-16 09:26:21 -0600

    menu responsive

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import BootstrapIcon from '@dvuckovic/vue3-bootstrap-icons';
+
+
+import './assets/style.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(BootstrapVue3).use(BootstrapIcon)
+
+app.mount('#app')
